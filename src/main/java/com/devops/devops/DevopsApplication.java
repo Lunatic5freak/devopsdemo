7799a24e9c989870342e8161d8db54de40d50ee6
Lunatic5freak <bbcmohanty.1999@gmail.com>
2022-09-03 02:48:21 +0530

    added endpoint

--- src/main/java/com/devops/devops/DevopsApplication.java
+++ src/main/java/com/devops/devops/DevopsApplication.java
@@ -14,6 +14,11 @@ public class DevopsApplication {
 		return "Hello World";
 	}
 
+	@GetMapping("/deploy")
+	public String getDeployMessage() {
+		return "Deploy";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsApplication.class, args);
 	}
